9c86edc63f1eb335335ada9dd50e1fdbc0af88c4
gnk263 <gnk263@users.noreply.github.com>
2018-08-31 23:29:19 +0900

    既存のparse処理に関するテストを追加 #10

--- house_control/test_parser.py
+++ house_control/test_parser.py
@@ -2,7 +2,23 @@ import unittest
 import parse_payload
 
 class TestParser(unittest.TestCase):
-    pass
+    def test_controll_aircon(self):
+        payload = "token=aaaaaaaaaaaaaaaaaaaaaaaa&team_id=bbbbbbbbb&team_domain=cccccc&channel_id=ddddddddd&channel_name=eeeeeeeeeeee&user_id=fffffffff&user_name=ggg&command=%2Fcontrol&text=aircon&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2Fhhhhhhhhh%2Fiiiiiiiiiiii%2Fjjjjjjjjjjjjjjjjjjjjjjjj"
+        expected = {
+            "token": "aaaaaaaaaaaaaaaaaaaaaaaa",
+            "team_id": "bbbbbbbbb",
+            "team_domain": "cccccc",
+            "channel_id": "ddddddddd",
+            "channel_name": "eeeeeeeeeeee",
+            "user_id": "fffffffff",
+            "user_name": "ggg",
+            "command": "%2Fcontrol",
+            "text": "aircon",
+            "response_url": "https%3A%2F%2Fhooks.slack.com%2Fcommands%2Fhhhhhhhhh%2Fiiiiiiiiiiii%2Fjjjjjjjjjjjjjjjjjjjjjjjj"
+        }
+        params = parse_payload.parse(payload)
+        self.assertDictEqual(expected, params)
+
 
 if __name__ == "__main__":
-    unittest.main();
+    unittest.main()
