d46fd2683654eac1f2f17175e019461677e7a6d9
gnk263 <gnk263@users.noreply.github.com>
2018-09-01 20:47:10 +0900

    parse処理を別メソッドに分離 #9

--- house_control/parse_payload.py
+++ house_control/parse_payload.py
@@ -2,15 +2,10 @@ import urllib.parse as url_parse
 
 
 def parse(payload):
-    params = {}
     format_type = _check_format(payload)
 
     if format_type == "slash_commands":
-        key_value_list = url_parse.unquote(payload.decode(encoding="utf-8")).split("&")
-        for item in key_value_list:
-            (key, value) = item.split("=")
-            params[key] = value
-        return params
+        return _parse_slash_commands(payload)
 
 
 def _check_format(payload):
@@ -19,3 +14,12 @@ def _check_format(payload):
         return "interactive_message"
     else:
         return "slash_commands"
+
+
+def _parse_slash_commands(payload):
+    params = {}
+    key_value_list = url_parse.unquote(payload.decode(encoding="utf-8")).split("&")
+    for item in key_value_list:
+        (key, value) = item.split("=")
+        params[key] = value
+    return params
