26efca5a934ddd2b4dbb4bdd3c93a5bf24aacb2c
gnk263 <gnk263@users.noreply.github.com>
2018-09-01 20:33:24 +0900

    parse時にフォーマットの種類の判定を追加 #9

--- house_control/parse_payload.py
+++ house_control/parse_payload.py
@@ -1,9 +1,21 @@
 import urllib.parse as url_parse
 
+
 def parse(payload):
     params = {}
-    key_value_list = url_parse.unquote(payload.decode(encoding="utf-8")).split("&")
-    for item in key_value_list:
-        (key, value) = item.split("=")
-        params[key] = value
-    return params
+    format_type = _check_format(payload)
+
+    if format_type == "slash-commands":
+        key_value_list = url_parse.unquote(payload.decode(encoding="utf-8")).split("&")
+        for item in key_value_list:
+            (key, value) = item.split("=")
+            params[key] = value
+        return params
+
+
+def _check_format(payload):
+    data = url_parse.unquote(payload.decode(encoding="utf-8"))
+    if data.startswith("payload="):
+        return "interactive_message"
+    else:
+        return "slash-commands"
